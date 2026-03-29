48054511ffac8c1e817692a08ebf3a0cfd70d941
kamthorn <kamthorn.sa@ksu.ac.th>
2026-03-29 11:05:16 +0700

    fix force

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
 export default async function CoursesPage() {
   const courses = await prisma.course.findMany({
     orderBy: { title: "asc" },
